8067d7ba1695ad5e606c53d7e6cd81734b44b6ea
Marco Mese <mese@na.infn.it>
2022-03-22 16:40:14 +0100

    Better error handling in main.c

--- main.c
+++ main.c
@@ -24,7 +24,7 @@
 #define DATA_ADDR        0x0E000000
 #define FIFO_DATA_LEN    2 // al momento leggo solo le prime due word che contengono il trg counter ed il gtu counter
 
-#define FILENAME_LEN     44
+#define FILENAME_LEN     50 // 44
 #define TRG_NUM_PER_FILE 25
 
 pthread_mutex_t mtx; // portare dentro cmdDecodeArgs_t e chkFifoArg_t e dichiararla in main
@@ -43,14 +43,14 @@ typedef struct chkFifoArgs{
     uint32_t* fifoData;
 } chkFifoArgs_t;
 
-void genFileName(uint32_t eventCounter, char* fileName, uint32_t fileNameLen){
+void genFileName(uint32_t fileCounter, char* fileName, uint32_t fileNameLen){
     time_t rawtime = time(NULL);
     struct tm *ptm = localtime(&rawtime);
 
     snprintf(fileName, fileNameLen, "/srv/ftp/clkb_event_%04d%02d%02d%02d%02d%02d-%04d.dat",
              ptm->tm_year + 1900, ptm->tm_mon + 1, ptm->tm_mday,
              ptm->tm_hour, ptm->tm_min, ptm->tm_sec,
-             eventCounter);
+             fileCounter);
 
     return;
 }
@@ -83,6 +83,7 @@ void *checkFifoThread(void *arg){
     chkFifoArgs_t* chkArg = (chkFifoArgs_t*)arg;
     uint16_t fifoDataCounter = 0;
     static uint32_t eventCounter = 0;
+    static uint32_t fileCounter = 0;
     char fileName[FILENAME_LEN] = "";
     int localSocketStatus;
     FILE *outFile;
@@ -103,7 +104,7 @@ void *checkFifoThread(void *arg){
             dma_transfer_s2mm(chkArg->regs->dmaReg, 128);
 
             if(!(eventCounter % TRG_NUM_PER_FILE))
-                genFileName(eventCounter,fileName,FILENAME_LEN);
+                genFileName(fileCounter++,fileName,FILENAME_LEN);
 
             printf("writing in %s\n",fileName);
 
@@ -127,21 +128,21 @@ void *checkFifoThread(void *arg){
 }
 
 int main(int argc, char *argv[]){
+    axiRegisters_t axiRegs;
+    cmdDecodeArgs_t cmdDecodeArg;
+    chkFifoArgs_t chkFifoArg;
     pthread_t cmdDecID;
     pthread_t chkSttID;
-    int threadErr;
     int listenfd = 0;
     int connfd = 0;
     struct sockaddr_in serv_addr;
-    cmdDecodeArgs_t cmdDecodeArg;
-    chkFifoArgs_t chkFifoArg;
-    uint32_t cmdDecRetVal;
-    uint32_t chkSttRetVal;
+    uint32_t* fifoData;
+    uint32_t cmdDecRetVal = 0;
+    uint32_t chkSttRetVal = 0;
     uint32_t cmdID = NONE;
     int socketStatus = 1;
-    axiRegisters_t axiRegs;
-    uint32_t* fifoData;
-    int err;
+    int err = -1;
+    int tries = 0;
 
     int devmem = open("/dev/mem", O_RDWR | O_SYNC);
     if (devmem < 0)
@@ -165,16 +166,22 @@ int main(int argc, char *argv[]){
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(CONN_PORT);
 
-    err = -1;
-    while(err < 0)
-        err = bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-        if(err < 0)
-            printf("\tERR: Error in bind: [%d]\n", err);
+    while(tries < BIND_MAX_TRIES){
+        err = bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+        if(err < 0){
+            printf("\tERR: Error in bind function: [%d]\nRetry...\n", err);
+            tries++;
+        }
+    }
 
+    tries = 0;
 
+    while(tries < LISTEN_MAX_TRIES){
     err = listen(listenfd, CONN_MAX_QUEUE);
     if(err < 0)
-        printf("\tERR: Error in listen: [%d]\n", err);
+        printf("\tERR: Error in listen function: [%d]\nRetry...\n", err);
+        tries++;
+    }
 
     cmdDecodeArg.regs = &axiRegs;
     cmdDecodeArg.cmdID = &cmdID;
@@ -187,24 +194,36 @@ int main(int argc, char *argv[]){
 
     while (1)
     {
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-        if(connfd < 0)
-            printf("\tERR: Error in accept: [%s]\n", strerror(err));
-
         cmdID = NONE;
+
+        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+        if(connfd < 0){
+            printf("\tERR: Error in accept: [%s]\n", strerror(err));
+            continue;
+        }
 
         cmdDecodeArg.connfd = connfd;
 
-        if(pthread_mutex_init(&mtx, NULL) != 0)
-            printf("\nERR: Cannot init mutex\n");
+        err = pthread_mutex_init(&mtx, NULL);
+        if(err != 0){
+            printf("\nERR: Cannot init mutex, disconnecting...: [%s]\n", strerror(err));
+            close(connfd);
+            continue;
+        }
 
-        threadErr = pthread_create(&cmdDecID, NULL, &cmdDecodeThread, (void*)&cmdDecodeArg);
-        if (threadErr != 0)
-            printf("\tERR: Cannot create cmdDecode thread: [%s]\n", strerror(threadErr));
+        err = pthread_create(&cmdDecID, NULL, &cmdDecodeThread, (void*)&cmdDecodeArg);
+        if(err != 0){
+            printf("\tERR: Cannot create cmdDecode thread, disconnecting...: [%s]\n", strerror(err));
+            close(connfd);
+            continue;
+        }
 
-        threadErr = pthread_create(&chkSttID, NULL, &checkFifoThread, (void*)&chkFifoArg);
-        if (threadErr != 0)
-            printf("\tERR: Cannot create checkFifo thread: [%s]\n", strerror(threadErr));
+        err = pthread_create(&chkSttID, NULL, &checkFifoThread, (void*)&chkFifoArg);
+        if(err != 0){}
+            printf("\tERR: Cannot create checkFifo thread, disconnecting...: [%s]\n", strerror(err));
+            close(connfd);
+            continue;
+        }
 
         pthread_join(cmdDecID, (void**)&cmdDecRetVal);
         pthread_join(chkSttID, (void**)&chkSttRetVal);
